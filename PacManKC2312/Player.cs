d0eb07345030fef852d6dcbcc411d28fa0043fc2
LyalenkovMA <167408417+LyalenkovMA@users.noreply.github.com>
2025-03-11 12:54:26 +0300

    Добавьте файлы проекта.

---
+++ PacManKC2312/Player.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PacManKC2312
+{
+    public class Player: GameObject
+    {
+        private int _direction;
+        private Vector _startPosition;
+
+        public Player(Vector position) : base(position, '@')
+        {
+            _direction = KayMoveRigth;
+            _startPosition = position;
+            CountLive = 3;
+        }
+
+        public int CountLive { get; private set; }
+
+        public bool IsLive => CountLive > 0;
+
+        public override void Update(Level level)
+        {
+            switch (_direction)
+            {
+                case KayMoveUp:
+                    SetPosition(new Vector(MoveStop, MoveBack), level);
+                    break;
+                case KayMoveDown:
+                    SetPosition(new Vector(MoveStop, MoveForward), level);
+                    break;
+                case KayMoveLeft:
+                    SetPosition(new Vector(MoveBack, MoveStop), level);
+                    break;
+                case KayMoveRigth:
+                    SetPosition(new Vector(MoveForward, MoveStop), level);
+                    break;
+            }
+        }
+
+        public void Control()
+        {
+            const ConsoleKey KeyUp = ConsoleKey.UpArrow;
+            const ConsoleKey KeyDown = ConsoleKey.DownArrow;
+            const ConsoleKey KeyRight = ConsoleKey.RightArrow;
+            const ConsoleKey KeyLeft = ConsoleKey.LeftArrow;
+
+            ConsoleKey key = Console.ReadKey().Key;
+
+            switch (key)
+            {
+                case KeyUp:
+                    _direction = KayMoveUp;
+                    break;
+                case KeyDown:
+                    _direction = KayMoveDown;
+                    break;
+                case KeyLeft:
+                    _direction = KayMoveLeft;
+                    break;
+                case KeyRight:
+                    _direction = KayMoveRigth;
+                    break;
+            }
+        }
+    }
+}
---
+++ PacManKC2312/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("PacManKC2312")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PacManKC2312")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("161ad031-044d-4cf0-bab3-587e34d2c2dd")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
