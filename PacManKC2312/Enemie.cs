9bfdc6b0f2a38b754c02041202ddd23082b3366d
LyalenkovMA <167408417+LyalenkovMA@users.noreply.github.com>
2025-03-14 12:09:25 +0300

    Добавьте файлы проекта.

---
+++ PacManKC2312/Enemie.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PacManKC2312
+{
+    public class Enemie : GameObject
+    {
+        public Enemie(Level level) : base(level.PositionEnemis, 'S')
+        {
+
+        }
+
+        public override void Update(Level level)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
